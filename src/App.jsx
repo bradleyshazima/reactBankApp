11abb14056cee55070d7036a38293368ced1409e
Bradley Shilton <bradyshilton@gmail.com>
2023-06-16 00:08:32 +0300

    Created the Navbar and started working on hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import styles from './style'
 import {   Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components'
 
 const App = () => (
-  <div className='bg-blue-gradient w-full overflow-hidden'>
+  <div className='bg-black w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-blue-gradient ${styles.flexStart}`}>
+    <div className={`bg-black ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-blue-gradient ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
